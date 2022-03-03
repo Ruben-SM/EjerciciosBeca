471c0b0772d3b450a7cfc2a08a6210f269c1bdbb
Ruben Saiz Molero <ruben.saiz@babelgroup.com>
2022-03-03 09:07:28 +0100

    Resta sin main

---
+++ Ejercicio1Git/Resta.java
@@ -0,0 +1,15 @@
+
+public class Resta {
+
+	private static int acc=0;
+	
+	public static int res(int a, int b) {
+		return a - b;
+	}
+	
+	public static int res(int a) {
+		return acc-=a;
+	}
+	
+
+}
